ecbd1316c563f28d8293ce06c68243e335251d15
najaram <arambulo.jan@gmail.com>
2019-11-25 17:49:22 +0800

    hangman autocorrect

--- foundations/io/hangman_project/lib/hangman.rb
+++ foundations/io/hangman_project/lib/hangman.rb
@@ -1,28 +1,24 @@
+# frozen_string_literal: true
+
 class Hangman
-  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+  DICTIONARY = ['cat', 'dog', 'bootcamp', 'pizza']
 
   def self.random_word
     DICTIONARY.sample
   end
 
   def initialize
-    @secret_word = Hangman::random_word
+    @secret_word = Hangman.random_word
     @guess_word = Array.new(@secret_word.length, '_')
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
 
-  def guess_word
-    @guess_word
-  end
+  attr_reader :guess_word
 
-  def attempted_chars
-    @attempted_chars
-  end
+  attr_reader :attempted_chars
 
-  def remaining_incorrect_guesses
-    @remaining_incorrect_guesses
-  end
+  attr_reader :remaining_incorrect_guesses
 
   def already_attempted?(char)
     attempted_chars.include?(char) ? true : false
@@ -31,14 +27,10 @@ class Hangman
   def get_matching_indices(char)
     indices = []
     @secret_word.each_char.with_index do |ele, i|
-      if ele == char
-        indices << i
-      end
+      indices << i if ele == char
     end
 
-    if indices.count == 0
-      return []
-    end
+    return [] if indices.count == 0
 
     indices
   end
@@ -50,11 +42,11 @@ class Hangman
   end
 
   def try_guess(char)
-    if self.already_attempted?(char)
+    if already_attempted?(char)
       puts 'that has already been attempted'
       return false
     end
-    
+
     @attempted_chars << char
 
     matched_indices = get_matching_indices(char)
@@ -62,7 +54,7 @@ class Hangman
     if matched_indices.empty?
       @remaining_incorrect_guesses -= 1
     else
-      self.fill_indices(char, matched_indices)
+      fill_indices(char, matched_indices)
     end
 
     true
@@ -71,33 +63,33 @@ class Hangman
   def ask_user_for_guess
     puts 'Enter a char:'
     input = gets.chomp
-    self.try_guess(input)
+    try_guess(input)
   end
 
   def win?
     if @secret_word == @guess_word.join('')
       puts 'WIN'
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
   def lose?
     if remaining_incorrect_guesses == 0
       puts 'LOSE'
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
   def game_over?
     if win? || lose?
       puts @secret_word
-      return true
+      true
     else
-      return false
+      false
     end
   end
 end

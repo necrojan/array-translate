da9744b0bf7cb596ccd74a89a98a1853e0cad3a5
najaram <arambulo.jan@gmail.com>
2019-12-09 18:00:51 +0800

    squaragonal

--- foundations/additional_projects/nauseating_problems.rb
+++ foundations/additional_projects/nauseating_problems.rb
@@ -161,30 +161,9 @@ end
 # meaning it's height is the same as it's width.
 def squaragonal?(arr)
   return true if (0...arr.length).collect { |row| arr[row][row] }.uniq.size <= 1
+  
+  len = arr.length
+  return true if (0...arr.length).collect { |row| arr[row][len -= 1] }.uniq.size <= 1
+  
+  false
 end
-
-p squaragonal?([
-    [:x, :y, :o],
-    [:x, :x, :x],
-    [:o, :o, :x],
-]) # true
-
-p squaragonal?([
-    [:x, :y, :o],
-    [:x, :o, :x],
-    [:o, :o, :x],
-]) # true
-
-# p squaragonal?([
-#     [1, 2, 2, 7],
-#     [1, 1, 6, 7],
-#     [0, 5, 1, 7],
-#     [4, 2, 9, 1],
-# ]) # true
-
-p squaragonal?([
-    [1, 2, 2, 5],
-    [1, 6, 5, 0],
-    [0, 2, 2, 7],
-    [5, 2, 9, 7],
-]) # false

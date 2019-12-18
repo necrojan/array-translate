062e0982ec5ae51d2604300955dde68851d2968b
najaram <arambulo.jan@gmail.com>
2019-12-18 18:11:21 +0800

    wip phase 3

--- foundations/additional_projects/perilous_procs.rb
+++ foundations/additional_projects/perilous_procs.rb
@@ -248,3 +248,35 @@ end
 
 # p proctition(['cat','boot', 'dog', 'bug', 'boat']) { |s| s[0] == 'b' }
 # # ["boot", "bug", "boat", "cat", "dog"]
+
+# Write a method selected_map! that accepts an array and two procs as arguments.
+#  The method should replace the elements that return true when passed into the 
+# first proc with their return values when they are passed into the second proc.
+# This method should mutate the input array and return nil.
+def selected_map!(arr, proc1, proc2)
+  arr.map! do |ele|
+    if proc1.call(ele)
+      proc2.call(ele)
+    else
+      ele
+    end
+  end
+  nil
+end
+
+# is_even = Proc.new { |n| n.even? }
+# is_positive = Proc.new { |n| n > 0 }
+# square = Proc.new { |n| n * n }
+# flip_sign = Proc.new { |n| -n }
+
+# arr_1 = [8, 5, 10, 4]
+# p selected_map!(arr_1, is_even, square)     # nil
+# p arr_1                                     # [64, 5, 100, 16]
+
+# arr_2 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_2, is_even, flip_sign)  # nil
+# p arr_2                                     # [10, -4, 7, -6, 2, -9]
+
+# arr_3 = [-10, 4, 7, 6, -2, -9]
+# p selected_map!(arr_3, is_positive, square) # nil
+# p arr_3                                     # [-10, 16, 49, 36, -2, -9]

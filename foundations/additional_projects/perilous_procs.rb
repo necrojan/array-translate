f9ad1f590d9868c061efbef691a7cd8573092d23
najaram <arambulo.jan@gmail.com>
2019-12-23 19:00:13 +0800

    procipher procs

--- foundations/additional_projects/perilous_procs.rb
+++ foundations/additional_projects/perilous_procs.rb
@@ -391,3 +391,57 @@ end
 
 # p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
 # # {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+
+# Write a method procipher that accepts a sentence and a hash as arguments. 
+# The hash contains procs as both keys and values. The method should return a 
+# new sentence where each word of the input sentence is changed by a value proc 
+# if the original word returns true when passed into the key proc. If an 
+# original word returns true for multiple key procs, then the value proc 
+# changes should be applied in the order that they appear in the hash.
+
+def procipher(sentence, hash)
+  words = sentence.split(' ')
+  new_words = words.map do |word|
+    new_word = word
+    hash.each do |proc_check, proc_change|
+      new_word = proc_change.call(new_word) if proc_check.call(word)
+    end
+    new_word
+  end
+  new_words.join(' ')
+end
+
+# is_yelled = Proc.new { |s| s[-1] == '!' }
+# is_upcase = Proc.new { |s| s.upcase == s }
+# contains_a = Proc.new { |s| s.downcase.include?('a') }
+# make_question = Proc.new { |s| s + '???' }
+# reverse = Proc.new { |s| s.reverse }
+# add_smile = Proc.new { |s| s + ':)' }
+
+
+# p procipher('he said what!',
+#     is_yelled => make_question,
+#     contains_a => reverse
+# ) # "he dias ???!tahw"
+
+# p procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => make_question
+# ) # "he dias !tahw???"
+
+# p procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => add_smile
+# ) # "he dias !tahw:)"
+
+# p procipher('stop that taxi now',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "stop that??? taxi??? now"
+
+# p procipher('STOP that taxi now!',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "STOP:) that??? taxi??? !won"

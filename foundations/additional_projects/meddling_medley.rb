5ddb6a16e94948003afa5c4080905c0c44280fed
najaram <arambulo.jan@gmail.com>
2020-01-07 13:38:17 +0800

    complete meddling medley

--- foundations/additional_projects/meddling_medley.rb
+++ foundations/additional_projects/meddling_medley.rb
@@ -169,3 +169,242 @@ end
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
 # p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
+# Write a method convert_pig_latin that accepts a sentence as an argument. 
+# The method should translate the sentence according to the following rules:
+
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the 
+# following rules:
+# if the word begins with a vowel, simply add 'yay' to the end of the word 
+# (example: 'eat'->'eatyay')
+# if the word begins with a non-vowel, move all letters that come before the 
+# word's first vowel to the end of the word and add 'ay' 
+# (example: 'trash'->'ashtray')
+# Note that if words are capitalized in the original sentence, they should 
+# remain capitalized in the translated sentence. 
+#Vowels are the letters a, e, i, o, u.
+
+def convert_pig_latin(sentence)
+  vowels = 'aeiouAEIOU'
+  new_sentence = []
+
+  words = sentence.split(' ')
+  words.each_with_index do |word, idx|
+    if vowels.include?(word[0])
+      new_sentence << word + 'yay'
+    elsif word.length < 3
+      new_sentence << word
+    else
+      new_sentence << modify(word, idx, vowels)
+    end
+  end
+  new_sentence.join(' ')
+end
+
+def modify(word, idx, vowels)
+  word.each_char.with_index do |char, i|
+    if vowels.include?(char)
+      if idx == 0
+        return word[i..-1].capitalize + word[0...i].downcase + 'ay'
+      else
+        first_letter = word[0...i]
+
+        if first_letter == first_letter.capitalize
+          return word[i..-1].capitalize + first_letter.downcase + 'ay'
+        else
+          return word[i..-1] + first_letter.downcase + 'ay'
+        end
+      end
+    end
+  end
+end
+
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
+# Write a method reverberate that accepts a sentence as an argument. 
+# The method should translate the sentence according to the following rules:
+# words that are shorter than 3 characters are unchanged
+# words that are 3 characters or longer are translated according to the 
+# following rules:
+# if the word ends with a vowel, simply repeat the word twice 
+# (example: 'like'->'likelike')
+# if the word ends with a non-vowel, repeat all letters that come after 
+# the word's last vowel, including the last vowel itself 
+# (example: 'trash'->'trashash')
+# Note that if words are capitalized in the original sentence, they should 
+# remain capitalized in the translated sentence. Vowels are the 
+# letters a, e, i, o, u.
+
+def reverberate(sentence)
+  vowels = 'aeiouAEIOU'
+  new_sentence = []
+
+  words = sentence.split(' ')
+  words.each.with_index do |word, idx|
+    if word.length < 3
+      new_sentence << word
+    else
+      new_sentence << modify_word(word, idx, vowels)
+    end
+  end
+  new_sentence.join(' ')
+end
+
+def modify_word(word, idx, vowels)
+  (0..word.length - 1).reverse_each do |i|
+    last_letter_vowel = word[-1]
+    if vowels.include?(last_letter_vowel)
+      if idx == 0 || word[0] == word[0].capitalize
+        return word.capitalize + word.downcase
+      else
+        return word.downcase * 2
+      end
+    else
+      if vowels.include?(word[i])
+        return word[0...i] + word[i..-1] * 2
+      end
+    end
+  end
+end
+
+# p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
+# p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
+# p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
+# p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance" 
+
+# Write a method disjunct_select that accepts an array and one or more procs 
+# as arguments. The method should return a new array containing the elements 
+# that return true when passed into at least one of the given procs.
+
+def disjunct_select(arr, *procs)
+  arr.select { |ele| procs.any? { |proc| proc.call(ele) } }
+end
+
+# longer_four = Proc.new { |s| s.length > 4 }
+# contains_o = Proc.new { |s| s.include?('o') }
+# starts_a = Proc.new { |s| s[0] == 'a' }
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+# ) # ["apple", "teeming"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o
+# ) # ["dog", "apple", "teeming", "boot"]
+
+# p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+#     longer_four,
+#     contains_o,
+#     starts_a
+# ) # ["ace", "dog", "apple", "teeming", "boot"]
+
+# Write a method alternating_vowel that accepts a sentence as an argument. 
+# The method should return a new sentence where the words alternate between 
+# having their first or last vowel removed. For example:
+# the 1st word should be missing its first vowel
+# the 2nd word should be missing its last vowel
+# the 3rd word should be missing its first vowel
+# the 4th word should be missing its last vowel
+# ... and so on
+# Note that words that contain no vowels should remain unchanged. 
+# Vowels are the letters a, e, i, o, u
+
+def alternating_vowel(sentence)
+  new_words = sentence.split(' ').map.with_index do |word, i|
+    i.even? ? remove_first_vowel(word) : remove_first_vowel(word.reverse).reverse
+  end
+  new_words.join(' ')
+end
+
+def remove_first_vowel(word)
+  vowels = 'aeiou'
+  (0..word.length - 1).each do |i|
+    if vowels.include?(word[i])
+      return word[0...i] + word[i + 1..-1]
+    end
+  end
+  word
+end
+
+# p alternating_vowel('panthers are great animals') # "pnthers ar grat animls"
+# p alternating_vowel('running panthers are epic') # "rnning panthrs re epc"
+# p alternating_vowel('code properly please') # "cde proprly plase"
+# p alternating_vowel('my forecast predicts rain today') # "my forecst prdicts ran tday"
+
+# Write a method silly_talk that accepts a sentence as an argument. 
+# The method should translate each word of the sentence according to the 
+# following rules:
+
+# if the word ends with a vowel, simply repeat that vowel at the end of the 
+# word (example: 'code'->'codee')
+# if the word ends with a non-vowel, every vowel of the word should be 
+# followed by 'b' and that same vowel (example: 'siren'->'sibireben')
+
+# Note that if words are capitalized in the original sentence, they should 
+# remain capitalized in the translated sentence. Vowels are the letters 
+# a, e, i, o, u.
+
+def silly_talk(sentence)
+  words = sentence.split(' ')
+  words.map { |word| add_b(word) }.join(' ')
+end
+
+def add_b(word)
+  vowels = 'AEIOUaeiou'
+  return word + word[-1] if vowels.include?(word[-1])
+  new_words = ''
+  (0..word.length - 1).each do |i|
+    char = word[i]
+    if vowels.include?(word[i])
+      new_words += char + 'b' + char.downcase
+    else
+      new_words += char
+    end
+  end
+  new_words
+end
+
+# p silly_talk('Kids like cats and dogs') # "Kibids likee cabats aband dobogs"
+# p silly_talk('Stop that scooter') # "Stobop thabat scobooboteber"
+# p silly_talk('They can code') # "Thebey caban codee"
+# p silly_talk('He flew to Italy') # "Hee flebew too Ibitabaly"
+
+# Write a method compress that accepts a string as an argument. The method 
+# should return a "compressed" version of the string where streaks of 
+# consecutive letters are translated to a single appearance of the letter 
+# followed by the number of times it appears in the streak. If a letter does 
+# not form a streak (meaning that it appears alone), then do not add a number 
+# after it.
+def compress(str)
+  i = 0
+  new_str = ''
+
+  while i < str.length
+    char = str[i]
+    count = 1
+    i += 1
+
+    while char == str[i]
+      count += 1
+      i += 1
+    end
+
+    if count > 1
+      new_str += char + count.to_s
+    else
+      new_str += char
+    end
+  end
+  new_str
+end
+
+p compress('aabbbbc')   # "a2b4c"
+p compress('boot')      # "bo2t"
+p compress('xxxyxxzzzz')# "x3yx2z4"

4b7fdd6bcfd8a384b65e5acc801685bd4d541c3e
necrojan <jan.dev1125@gmail.com>
2020-05-08 21:50:11 +0800

    add enumerables-array

---
+++ ruby/enumerables.rb
@@ -0,0 +1,61 @@
+class Array
+  def my_each(&proc)
+    length.times do |num|
+      proc.call(self[num])
+    end
+    self
+  end
+
+  def my_select(&proc)
+    select = []
+
+    my_each do |ele|
+      select << ele if proc.call(ele)
+    end
+    select
+  end
+
+  def my_reject(&proc)
+    new_arr = []
+    my_each do |ele|
+      new_arr << ele unless proc.call(ele)
+    end
+    
+    new_arr
+  end
+
+  def my_any?(&proc)
+    my_each do |ele|
+      return true if proc.call(ele)
+    end
+    false
+  end
+
+  def my_all?(&proc)
+    my_each do |ele|
+      return false unless proc.call(ele)
+    end
+
+    true
+  end
+
+  def my_flatten
+    flattened = []
+
+    my_each do |ele|
+      if ele.is_a? Array
+        flattened.concat(ele.my_flatten)
+      else
+        flattened << ele
+      end
+    end
+
+    flattened
+  end
+
+  def my_zip(*args)
+    height = args[0].length
+    height
+  end
+end
+
